261c7dd1ad9046d5460618588fe70b3c0fc66bf0
Lean Junio <leanjunio@outlook.com>
2023-06-03 19:04:47 -0400

    update

--- import.sh
+++ import.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
-jq '.data' roster.json > data.json
-mongoimport --host mongodb --db rebel-db --collection artists --type json --file data.json --jsonArray
+echo "Cleaning db..."
+mongosh rebel-db --host mongodb --eval "db.artists.deleteMany({})"
+echo "DB Ready for seeding..."
+
+echo "Seeding Rebel DB..."
+jq '.data' /roster.json > /data.json
+mongoimport --host mongodb --db rebel-db --collection artists --type json --file /data.json --jsonArray
+echo "Seeding Done..."
